0f7e028d895e5ec162b928a9ab8ee7b1c5bffbed
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-04-10 12:25:47 -0400

    Finished mvp

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const Header = () => {
+  //create the elements
+  const headerContainer = document.createElement('div');
+  const date = document.createElement('span');
+  const title = document.createElement('h1');
+  const temp = document.createElement('span');
+
+  //Add class
+  headerContainer.classList.add('header');
+  date.classList.add('date');
+  temp.classList.add('temp');
+
+  //Add content
+  date.textContent = 'March 28, 2019';
+  title.textContent = 'Lambda Times';
+  temp.textContent = '98°';
+
+  //Nest them properly
+  headerContainer.appendChild(date);
+  headerContainer.appendChild(title);
+  headerContainer.appendChild(temp);
+  return headerContainer;
+};
+
+document.querySelector('.header-container').appendChild(Header());
